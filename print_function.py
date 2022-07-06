eef34bc8c1ec0c7c8652438e59b0b62d80ddcd8e
Dude <dude1410@mail.ru>
2022-07-06 15:50:59 +0300

    + strings lecture

--- print_function.py
+++ print_function.py
@@ -4,3 +4,20 @@ import platform
 print('hello, world')
 print(sys.version)
 print(platform.release())
+
+print("привет".encode())
+
+print(b'\xd0\xbf\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82'.decode())
+
+str_test = "qwerty"
+print(str_test[0])
+print(str_test[-1])
+print(str_test[1:4])
+print(str_test[:4])
+print(str_test[:])
+
+str_test2 = "qwertyuiopasdfghjkl"
+print(str_test2[1:15:3])
+print(len(str_test2))
+print(str_test2[::-1])
+print(str_test2[18:0:-1])
