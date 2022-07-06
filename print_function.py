d6441e325dc4242cde50ae01e30dd1a58e9134ca
dude <dude1410@mail.ru>
2022-07-06 20:47:56 +0300

    2.2 block finished

--- print_function.py
+++ print_function.py
@@ -21,3 +21,30 @@ print(str_test2[1:15:3])
 print(len(str_test2))
 print(str_test2[::-1])
 print(str_test2[18:0:-1])
+
+a, b, c = "word1", 12.005, 123
+
+print(a, b, c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(isinstance(b, float))
+
+print(id(a))
+
+# x, y = map(int, input().split()) # ожидание ввода от нас двух интовых значений через запятую
+# print(type(x))
+# print(type(y))
+# print(x, y)
+
+# методы строк
+car = "Тойота Королла"
+print(len(car))
+print(car.upper())
+print(car.lower())
+print(car.isalpha()) # проверка, все ли знаки буквы
+print(car.find('й'))
+print(car.rfind('о')) # только первое значение с головы
+print(car.rfind('о')) # только первое значение с хвоста
+print(car.replace('о', '0'))
