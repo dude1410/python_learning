eec89358c0393e8c1ea7de3991d27af782a593f0
dude <dude1410@mail.ru>
2022-07-26 23:19:21 +0300

    new skillbox lesson 3-4 finished

---
+++ newskillbox/3-4-lesson.py
@@ -0,0 +1,18 @@
+# 1
+first_int = int(input("Введите первое число: "))
+second_int = int(input("Введите второе число: "))
+print(first_int + second_int)
+
+# ************************
+# 2
+third_int = int(input("Введите третье число: "))
+fourth_int = int(input("Введите четвертое число: "))
+print(2 * (third_int + 5 + (first_int * second_int) / (4 * second_int)) * (fourth_int - (2 * first_int ** 3) / 30) - 10)
+
+# *****************************
+# 3
+a = '2'
+b = '5'
+c = '3'
+num = 6 ** int(a) + (7 - int(b)) * int(c)
+print(num)
