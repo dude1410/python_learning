8238c78de0eeaf807ef29ad364863a8181b8b454
dude <dude1410@mail.ru>
2022-07-26 22:39:39 +0300

    new skillbox lesson 2-6 finished

---
+++ newskillbox/2-6-lesson.py
@@ -0,0 +1,48 @@
+# 1
+client = "Петя"
+print(client)
+print(" и ")
+pet = "Кот"
+print(pet)
+
+# *******************************
+# 2
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+
+print(r, b, g, r + g + b, b, g + b)
+
+# *********************************
+# 3
+first_animal = "Заяц"
+second_animal = "Черепаха"
+print(first_animal + " спит, " + second_animal + " идет")
+
+# ****************************
+# 4
+first_name = input('Введите имя пользователя: ')
+greeting = 'Утро доброе,'
+print(greeting, first_name)
+intro = "К сожалению, у Вас нет доступа к системе."
+info = "Пожалуйста, обратитесь к системному администратору."
+print(intro)
+print(info)
+
+# *****************************
+# 5
+city = input("Введите город: ")
+country = input("Введите страну: ")
+print(country + " - " + city)
+
+# *******************************
+# 6
+first_word = input('Введите первое слово: ')
+second_word = input('Введите второе слово: ')
+print(first_word, second_word)
+
+temp = first_word
+first_word = second_word
+second_word = temp
+
+print(first_word, second_word)
