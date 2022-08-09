cac2222bc0b969fb2e4c449e42aeae0e72f08a5c
dude <dude1410@mail.ru>
2022-08-09 19:07:47 +0300

    new skillbox lesson 5-4

---
+++ newskillbox/5-4-lesson.py
@@ -0,0 +1,25 @@
+# 1
+# year = int(input('Введите год выпуска: '))
+# speed_amount = int(input('Введите кол-во скоростей: '))
+#
+# if year < 2018 or speed_amount < 24:
+#     print('Не катит')
+# else:
+#     print('Годнота')
+
+# ///////////////////////////////////////////////
+# 2
+# point = int(input('Сколько баллов набрал: '))
+# gold_medal = int(input('Есть медаль? '))
+# if point >= 280 and gold_medal == 1:
+#     print('Поздравляем! Ты поступил!')
+# else:
+#     print('К сожалению, ты не прошёл в наш университет.')
+
+# /////////////////////////////////////////////////
+# 3
+temperature = int(input('Enter temperature: '))
+if 0 < temperature < 100:
+    print('IT\'S OK')
+else:
+    print("WARNING!")
